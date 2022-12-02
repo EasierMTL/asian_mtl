dbdafcad2f936ce450814b9f5b54843b52967f7b
Joseph Chen <jchen42703@gmail.com>
2022-12-01 22:22:25 -0500

    chore: adapted evaluation scripts to use the relative data

--- scripts/evaluate_quantized_dynamic.py
+++ scripts/evaluate_quantized_dynamic.py
@@ -1,4 +1,5 @@
 import torch
+import os
 from novel_tl_models.models.base import ChineseToEnglishTranslator
 from novel_tl_models.evaluation.evaluation_pipeline import EvaluationPipeline
 
@@ -11,14 +12,19 @@ if __name__ == "__main__":
                                                            dtype=torch.qint8)
     print("Loaded quantized model")
     pipeline = EvaluationPipeline(predictor=translator)
-    pipeline.load_dset("./test_data/opus-2020-07-17.test.txt")
+    file_path = os.path.abspath(__file__)
+    cli_dir = os.path.dirname(file_path)
+    pipeline.load_dset(
+        os.path.join(cli_dir, "test_data/opus-2020-07-17.test.txt"))
 
     now = datetime.now()
 
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
-    # pipeline.evaluate(num_workers=4, sentence_bleu=False)
-    pipeline.evaluate(num_workers=4, sentence_bleu=False, print_bleu_every=1000)
+    pipeline.evaluate(max_samples=100,
+                      num_workers=4,
+                      sentence_bleu=False,
+                      print_bleu_every=1000)
 
     after = datetime.now()
     after_time = after.strftime("%H:%M:%S")
